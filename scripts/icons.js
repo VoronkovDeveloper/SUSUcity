92bf1dd0ea0933dd8c51a5266ad6bcef6c63ecc3
Max Choy <m.choy@gosu.kz>
2024-04-15 23:39:35 +0600

    chore: structure refactor

---
+++ scripts/icons.js
@@ -0,0 +1,39 @@
+let susu, food, home, lib, tree, memorial;
+DG.then(function() {
+    susu = DG.icon({
+        iconUrl: 'assets/images/icons/susu.png',
+        iconSize: [40, 40],
+        iconAnchor: [20, 40],
+        popupAnchor: [0, -40]
+    });
+    food = DG.icon({
+        iconUrl: 'assets/images/icons/food.png',
+        iconSize: [40, 40],
+        iconAnchor: [20, 40],
+        popupAnchor: [0, -40]
+    });
+    home = DG.icon({
+        iconUrl: 'assets/images/icons/home.png',
+        iconSize: [40, 40],
+        iconAnchor: [20, 40],
+        popupAnchor: [0, -40]
+    });
+    lib = DG.icon({
+        iconUrl: 'assets/images/icons/lib.png',
+        iconSize: [40, 40],
+        iconAnchor: [20, 40],
+        popupAnchor: [0, -40]
+    });
+    tree = DG.icon({
+        iconUrl: 'assets/images/icons/tree.png',
+        iconSize: [40, 40],
+        iconAnchor: [20, 40],
+        popupAnchor: [0, -40]
+    });
+    memorial = DG.icon({
+        iconUrl: 'assets/images/icons/memorial.png',
+        iconSize: [40, 40],
+        iconAnchor: [20, 40],
+        popupAnchor: [0, -40]
+    });
+}  );
